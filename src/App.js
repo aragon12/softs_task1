97c7e1b1d400eb3f7beeb0a28d5ffe030b2881c6
aragon12 <sm95007@gmail.com>
2021-08-15 12:36:49 +0530

    zoomapp: make UI responsive
    for mobile devices

--- src/App.js
+++ src/App.js
@@ -21,121 +21,118 @@ function App() {
 
   const fixH = 480;
   const toolTipDelay = 700;
-
-const isImage = (file) => {
-  return file['type'].split('/')[0]==='image';
-}
-
-const uploadHandler = (e) => {
- if(!e.target.files.length){
-   setIsOpen(false);
-   setShowControls(false);
-   setShowImg(false);
-   setImgsrc(undefined);
-    return;
+  
+  const isImage = (file) => {
+    return file['type'].split('/')[0]==='image';
   }
 
-  const file = e.target.files[0];
-  if(!isImage(file)) {
+  const uploadHandler = (e) => {
+   if(!e.target.files.length) {
+     setIsOpen(false);
+     setShowControls(false);
+     setShowImg(false);
+     setImgsrc(undefined);
+     return;
+    }
+
+    const file = e.target.files[0];
+    if(!isImage(file)) {
+      setIsOpen(false);
+      setShowControls(false);
+      setShowImg(false);
+      setImgsrc(undefined);
+      setAlert(true);
+      return;
+    }
+
+    setShowImg(false);
+    setIsOpen(true);
+    setAlert(false);
+    setImgsrc(URL.createObjectURL(file))
+  }
+
+  const imgResize = (natH, natW, newH) => {
+    return ~~((newH * natW)/natH);
+  }
+  
+  const openHandler = (e) => {
+    if(!imgSrc) {
+      return;
+    }
+    var i = new Image();
+    i.src = imgSrc;
+    i.onload = () => {
+      setImgsrcH(fixH);
+      setImgsrcW(imgResize(i.naturalHeight, i.naturalWidth, fixH));
+    }
+    setImgRotation(0);
     setIsOpen(false);
+    setShowImg(true);
+    setShowControls(true);
+  }
+
+  const closeHandler = (e) => {
     setShowControls(false);
     setShowImg(false);
-    setImgsrc(undefined);
-    setAlert(true);
-    return;
+    setIsOpen(true);
   }
 
-  setShowImg(false);
-  setIsOpen(true);
-  setAlert(false);
-  setImgsrc(URL.createObjectURL(file))
-}
-
-const imgResize = (natH, natW, newH) => {
-  return ~~((newH * natW)/natH);
-}
-
-const openHandler = (e) => {
-  if(!imgSrc) {
-    return;
+  const zoomHandler = (fact) => (e) => {
+    const newH = imgSrcH + fact;
+    const newW = imgResize(imgSrcH, imgSrcW, newH);
+    setImgsrcH(newH);
+    setImgsrcW(newW);
   }
-  var i = new Image();
-  i.src = imgSrc;
-  console.log(i);
-  i.onload = () => {
-    setImgsrcH(fixH);
-    setImgsrcW(imgResize(i.naturalHeight, i.naturalWidth, fixH));
+
+  const rotateHandler = (angle) => (e) => {
+    const newImgRotation = imgRotation + angle;
+    setImgRotation(newImgRotation);
   }
-  setImgRotation(0);
-  setIsOpen(false);
-  setShowImg(true);
-  setShowControls(true);
-}
-
-const closeHandler = (e) => {
-  setShowControls(false);
-  setShowImg(false);
-  setIsOpen(true);
-}
-
-const zoomHandler = (fact) => (e) => {
-  const newH = imgSrcH + fact;
-  const newW = imgResize(imgSrcH, imgSrcW, newH);
-  setImgsrcH(newH);
-  setImgsrcW(newW);
-}
-
-const rotateHandler = (angle) => (e) => {
-  const newImgRotation = imgRotation + angle;
-  setImgRotation(newImgRotation);
-}
 
   return (
           <div className="maincont">
             <div class="inst_cont">
-              <span style={{paddingRight: '24px'}}>To start, first upload an image:</span>
+              <span style={{paddingRight: '50px'}}>To start, first upload an image:</span>
               <input type="file" accept="image/*" onChange={uploadHandler} />
               <Button color="primary" onClick={openHandler} disabled={!isOpen} >Open</Button>
               {showImg && <Button color="primary" onClick={closeHandler} >Close</Button>}<br />
-              </div>
+            </div>
 
-              <div className="img_cont" >
-                <div className="img_frame" >
-                  <Collapse in={alert}>
-                    <Alert severity="error">Not an image file</Alert>
-                  </Collapse>
-                  {showImg && <img alt="" style={{transform: `rotate(${imgRotation}deg)`}} src={imgSrc} height={imgSrcH} width={imgSrcW} />}
-                </div>
-              </div>
-              <div className="control_cont" >
+            <div className="img_frame" >
+              <Collapse in={alert}>
+                <Alert severity="error">Not an image file</Alert>
+              </Collapse>
+              {showImg && <img alt="" style={{transform: `rotate(${imgRotation}deg)`}} src={imgSrc} height={imgSrcH} width={imgSrcW} />}
+            </div>
+            <div className="control_cont">
               <Tooltip title="Rotate Left" enterDelay={toolTipDelay}>
-                  <IconButton color="primary" onClick={rotateHandler(-90)} disabled={!showControls}>
-                    <RotateLeftIcon fontSize="large" />
-                  </IconButton>
-                </Tooltip>
+                <IconButton color="primary" onClick={rotateHandler(-90)} disabled={!showControls}>
+                  <RotateLeftIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
               <Tooltip title="Rotate Right" enterDelay={toolTipDelay}>
-                  <IconButton color="primary" onClick={rotateHandler(90)} disabled={!showControls}>
-                    <RotateRightIcon fontSize="large" />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Zoom in" enterDelay={toolTipDelay}>
-                  <IconButton color="primary" onClick={zoomHandler(50)} disabled={!showControls}>
-                    <ZoomInIcon fontSize="large" />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Zoom out" enterDelay={toolTipDelay}>
-                  <IconButton color="primary" onClick={zoomHandler(-50)} disabled={!showControls}>
-                    <ZoomOutIcon fontSize="large" />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Restore" enterDelay={toolTipDelay}>
-                  <IconButton color="primary" onClick={zoomHandler(fixH - imgSrcH)} disabled={!showControls}>
-                    <YoutubeSearchedForIcon fontSize="large" />
-                  </IconButton>
-                </Tooltip>
-              </div>
+                <IconButton color="primary" onClick={rotateHandler(90)} disabled={!showControls}>
+                  <RotateRightIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Zoom in" enterDelay={toolTipDelay}>
+                <IconButton color="primary" onClick={zoomHandler(50)} disabled={!showControls}>
+                  <ZoomInIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Zoom out" enterDelay={toolTipDelay}>
+                <IconButton color="primary" onClick={zoomHandler(-50)} disabled={!showControls}>
+                  <ZoomOutIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Restore" enterDelay={toolTipDelay}>
+                <IconButton color="primary" onClick={zoomHandler(fixH - imgSrcH)} disabled={!showControls}>
+                  <YoutubeSearchedForIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+            </div>
           </div>
-  );
+         );
 }
 
 export default App;
